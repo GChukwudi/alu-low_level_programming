963c6b366271743ede426620543cfb7104ac02e4
GChukwudi <g.chukwudi@alustudent.com>
2024-09-12 11:03:07 -0700

    updated

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Result: 0 for succes
+ */
+
+int main(voide)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+
+	putchar('\n');
+
+	return (0);
+}
