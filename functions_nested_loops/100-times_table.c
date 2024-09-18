faa77a6464fd093fbb0d2613acb66123ed949e70
GChukwudi <g.chukwudi@alustudent.com>
2024-09-18 12:58:56 +0200

    updated

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -23,9 +23,9 @@ void print_times_table(int n)
             {
                 _putchar(',');
                 _putchar(' ');
-                if (result < 10)
+                if (result =< 10)
                     _putchar(' ');
-                if (result < 100)
+                if (result =< 100)
                     _putchar(' ');
             }
             if (result < 10)
