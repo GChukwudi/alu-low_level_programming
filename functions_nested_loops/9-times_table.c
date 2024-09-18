972fdfde9fd8e418766dfbe6195e9286fa7cb4c3
GChukwudi <g.chukwudi@alustudent.com>
2024-09-18 12:27:15 +0200

    updated

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,32 +10,30 @@ void times_table(void)
 {
 	int i, j, n;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
-		for (j = 1; j < 10; j++)
+		for (j = 1; j < 9; j++)
 		{
-			n = i * j;
-			if (n < 10)
-			{
-				_putchar(' ');
-
-			    _putchar(n + '0');
-			}
-			else
-			{
-				// _putchar(' ');
-				_putchar(n / 10 + '0');
-				_putchar(n % 10 + '0');
-			}
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
-	}
+             n = i * j;
+             if (j != 0)
+             {
+                 _putchar(',');
+                 _putchar(' ');
+                if (n < 10)
+                {
+                    _putchar(' ');
+                }
+             }
+            if (n < 10)
+            {
+                _putchar(n + '0');
+            }
+            else
+            {
+                _putchar((n / 10) + '0');
+                _putchar((n % 10) + '0');
+            }
+        }
+         _putchar('\n');
+    }
 }
