a1be257d30b81fb17dc9609d807381bbb98e8fc5
GChukwudi <g.chukwudi@alustudent.com>
2024-09-18 12:31:35 +0200

    updated

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ void times_table(void)
 {
 	int i, j, n;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 1; j < 9; j++)
+		for (j = 1; j < 10; j++)
 		{
 			n = i * j;
 			if (j != 0)
