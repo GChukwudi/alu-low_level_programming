999749169e6a2985faa9b62d8395d54935d2fd1c
GChukwudi <g.chukwudi@alustudent.com>
2024-09-18 13:27:50 +0200

    updated

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,33 +7,33 @@
 
 void print_times_table(int n)
 {
-	int i, j, result;
+    int i, j, res;
 
-	if (n < 0 || n > 15)
-		return;
+    if (n > 15 || n < 0)
+        return;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			result = i * j;
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				if (result <= 10)
-					_putchar(' ');
-				else if (result <= 100)
-					_putchar(' ');
-			}
-			if (result < 10)
-				_putchar(result + '0');
-			else
-			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            res = i * j;
+            if (j == 0)
+                _putchar('0');
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                if (res / 100 == 0)
+                    _putchar(' ');
+                else
+                    _putchar((res / 100) + '0');
+                if (res / 10 == 0)
+                    _putchar(' ');
+                else
+                    _putchar(((res / 10) % 10) + '0');
+                _putchar((res % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
