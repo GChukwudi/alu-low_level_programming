0e84d7ee296440ad0c72d372f51c187c499a602d
GChukwudi <g.chukwudi@alustudent.com>
2024-10-01 16:44:16 +0200

    pointer

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: pointer to the string
+ *
+ * Return: len of string
+ */
+
+int _strlen(char *s)
+{
+int length;
+
+while (s[length != 0])
+{
+length++;
+}
+return length;
+}
