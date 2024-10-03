b8ffb8d4e7e2f6825c3038014d24b566c43599f8
GChukwudi <g.chukwudi@alustudent.com>
2024-10-03 15:34:16 +0200

    updated

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to the string to be printed
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+    int len;
+
+    for (len = 0; str[len] != '\0'; len++)
+    {
+        if (len % 2 == 0)
+        {
+            _putchar(str[len]);
+        }
+    }
+
+    _putchar('\n');
+}
