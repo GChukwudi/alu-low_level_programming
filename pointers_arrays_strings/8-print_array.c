a548a2b769703a5464e6c5b9187de346751a63d4
GChukwudi <g.chukwudi@alustudent.com>
2024-10-03 18:03:38 +0200

    updated

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -14,14 +14,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i] + '0');
-
-		if (i < n - 1)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
