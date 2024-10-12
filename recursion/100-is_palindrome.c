63d32c4a2364cf0ff9bfaa50355c8e183bb1db39
GChukwudi <g.chukwudi@alustudent.com>
2024-10-12 08:07:07 +0200

    updated

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -4,12 +4,34 @@
  * is_palindrome - checks if a string is a palindrome
  * @s: string to check
  *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * Return: 1 if the string is a palindrome, 0 otherwise
  */
+
 int is_palindrome(char *s)
 {
-	int i = 0;
-	int j = _strlen_recursion(s) - 1;
+	int len = _strlen_recursion(s);
 
-	return (check(s, i, j));
+	return (is_palindrome_helper(s, 0, len - 1));
+}
+
+int is_palindrome_helper(char *s, int i, int j)
+{
+	if (i >= j)
+	{
+		return (1);
+	}
+	if (s[i] != s[j])
+	{
+		return (0);
+	}
+	return (is_palindrome_helper(s, i + 1, j - 1));
+}
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
 }
--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,44 +1,34 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to find the length of
- * Return: length of the string
- */
-
-int _strlen_recursion(char *s)
-{
-    if (*s == '\0')
-        return (0);
-    return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * is_palindrome_helper - checks if a string is a palindrome
- * @s: string to check
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: 1 if palindrome, 0 if not
+ * Return: 1 if the strings can be considered identical, 0 otherwise
  */
 
-int is_palindrome_helper(char *s, int i, int j)
+int wildcmp(char *s1, char *s2)
 {
-    if (i >= j)
-        return (1);
-    if (s[i] != s[j])
-        return (0);
-    return (is_palindrome_helper(s, i + 1, j - 1));
-}
-
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: string to check
- *
- * Return: 1 if palindrome, 0 if not
- */
-
-int is_palindrome(char *s)
-{
-    int len = _strlen_recursion(s);
-
-    return (is_palindrome_helper(s, 0, len - 1));
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	return (0);
 }
