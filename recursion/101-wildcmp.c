33ecf39d936d703b0f9d6154da5e7bfb500c12dd
GChukwudi <g.chukwudi@alustudent.com>
2024-10-12 07:51:11 +0200

    updated

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,34 +1,52 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: 1 if the strings can be considered identical, 0 otherwise
+* _strlen_recursion - returns the length of a string
+* @s: string
+
+* Return: length of string
+*/
+int _strlen_recursion(char *s)
+{
+    if (s[0] != '\0')
+    {
+        return (1 + _strlen_recursion(s + 1));
+    }
+    return (0);
+}
+
+/**
+ * check_palindrome - checks if a string is a palindrome
+ * @s: string
+ * @i: start index
+ * @j: end index
+ * 
+ * Return: 1 if palindrome, 0 otherwise
  */
 
-int wildcmp(char *s1, char *s2)
+int check_palindrome(char *s, int i, int j)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (1);
-	}
-	if (*s1 == *s2)
-	{
-		return (wildcmp(s1 + 1, s2 + 1));
-	}
-	if (*s2 == '*')
-	{
-		if (*(s2 + 1) == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		if (*s1 == '\0')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-	return (0);
+    if (i >= j)
+    {
+        return (1);
+    }
+    if (s[i] != s[j])
+    {
+        return (0);
+    }
+    return (check_palindrome(s, i + 1, j - 1));
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string
+ * 
+ * Return: 1 if palindrome, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+    int len = _strlen_recursion(s);
+
+    return (check_palindrome(s, 0, len - 1));
 }
