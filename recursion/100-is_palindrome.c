1f29dcc0f8676f95875dc2fea72badf415bb1508
GChukwudi <g.chukwudi@alustudent.com>
2024-10-12 08:10:36 +0200

    updated

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -14,6 +14,14 @@ int is_palindrome(char *s)
 	return (is_palindrome_helper(s, 0, len - 1));
 }
 
+/**
+ * is_palindrome_helper - checks if a string is a palindrome
+ * @s: string to check
+ * @i: start index
+ * @j: end index
+ *
+ * Return: 1 if the string is a palindrome, 0 otherwise
+ */
 int is_palindrome_helper(char *s, int i, int j)
 {
 	if (i >= j)
@@ -27,6 +35,12 @@ int is_palindrome_helper(char *s, int i, int j)
 	return (is_palindrome_helper(s, i + 1, j - 1));
 }
 
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to check
+ *
+ * Return: length of the string
+ */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
