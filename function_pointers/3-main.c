25244a7b5dd019f78e4385d1335a7f85803af4d3
GChukwudi <g.chukwudi@alustudent.com>
2024-11-05 16:28:18 +0200

    updated

---
+++ function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	result = operation(a, b);
+	printf("%d\n", result);
+
+	return (0);
+}
