ca9d9fa9a4e3ca6fbea03532ca5901c9c79cf40d
GChukwudi <g.chukwudi@alustudent.com>
2024-11-10 12:08:20 +0200

    updated

---
+++ file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+
+
+
+ssize_t read_file(const char *filename, char **buffer);
+
+
+#endif /* MAIN_H */
