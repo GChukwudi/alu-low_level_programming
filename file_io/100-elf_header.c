25d438ecf2d4ed313a3543b1e23399a1c340a043
GChukwudi <g.chukwudi@alustudent.com>
2024-11-10 12:49:02 +0200

    updated

--- file_io/100-elf_header.c
+++ file_io/100-elf_header.c
@@ -289,6 +289,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	check_elf(header->e_ident);
+	printf("ELF Header:\n");
 	print_magic(header->e_ident);
 	print_class(header->e_ident);
 	print_data(header->e_ident);
